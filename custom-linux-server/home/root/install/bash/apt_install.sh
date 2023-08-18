9964f05d3ebda038de55d50b7a43024fbbee51ee
C-Keenan <108506379+C-Keenan@users.noreply.github.com>
2023-08-18 02:19:12 -0400

    commit changes

--- custom-linux-server/home/root/install/bash/apt_install.sh
+++ custom-linux-server/home/root/install/bash/apt_install.sh
@@ -8,13 +8,15 @@ apt install tasksel -y
 apt install lamp-server -y
 apt install curl -y
 apt install apache2 -y
+apt install php libapache2-mod-php -y
+apt install mysql-server -y
+apt install libapache2-mod-auth-pysql phpmyadmin -y
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 echo 'FROM php:7.4.3-alpine' > php.Dockerfile
 docker-php-ext-install mysqli pdo pdo_mysql
 cd ~/docker/lamp
 touch docker-compose.yml
-
 apt install ufw -y
 mkdir -p /home/root/var/www/html/game-guides-and-how-tos
 mkdir -p /home/root/var/www/html/images
@@ -35,6 +37,7 @@ cp /home/root/var/www/html/projects.html /var/www/html/projects.html
 cp /home/root/var/www/html/recipes.html /var/www/html/recipes.html
 cp /home/root/var/www/html/videos.html /var/www/html/videos.html
 ufw allow 80
+ufw allow 443
 ufw enable -y
 systemctl enable apache2
 exit
