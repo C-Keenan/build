fb974dbaedaf6b29e35bac7245bcb7c4958b39f6
C-Keenan <108506379+C-Keenan@users.noreply.github.com>
2023-08-21 14:37:58 -0400

    commit changes

--- custom-linux-server/bash/apt_install.sh
+++ custom-linux-server/bash/apt_install.sh
@@ -6,5 +6,5 @@ apt autoremove -y
 apt install systemctl -y
 apt install nginx -y
 systemctl enable nginx
-mv /home/root/etc/apache2/sites-available/test_nginx.conf /etc/nginx/nginx.conf
+mv /home/root/etc/apache2/sites-available/nginx.conf /etc/nginx/nginx.conf
 exit
